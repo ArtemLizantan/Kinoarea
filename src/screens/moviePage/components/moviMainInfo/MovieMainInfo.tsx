a3a95d4ee4dc692394c9484b21a7c93f51612ff9
Artem <plishkan25@gmail.com>
2024-04-05 22:11:55 +0300

    add adaptive movie page

--- src/screens/moviePage/components/moviMainInfo/MovieMainInfo.tsx
+++ src/screens/moviePage/components/moviMainInfo/MovieMainInfo.tsx
@@ -8,6 +8,7 @@ import styles from "./mainInfoCard.module.scss";
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 import { useState } from "react";
 import Popup from "../../../../components/popup/Popup";
+import useResizeObserver from "use-resize-observer";
 
 const icons: IIcons[] = [
   { name: <SlSocialVkontakte />, path: "/", id: 1 },
@@ -26,6 +27,9 @@ const MainInfoCard = ({
   trailer,
 }: IMainInfoCardProps) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { ref, width } = useResizeObserver<HTMLDivElement>();
+  const tablet = width !== undefined ? width <= 800 : false;
+  const mobile = width !== undefined ? width <= 768 : false;
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -33,15 +37,18 @@ const MainInfoCard = ({
 
   return (
     <div className={styles.cardInfo}>
-      <div className={styles.cardInfo__background}>
+      <div ref={ref} className={styles.cardInfo__background}>
         <div className={styles.cardInfo__afterOpacity}></div>
         <div className={styles.cardInfo__beforeOpacity}></div>
         <img src={backGroundPoster} alt="" />
       </div>
       <div className={styles.cardInfo__body}>
-        <div className={styles.cardInfo__left}>
-          <img src={poster} alt="poster" />
-        </div>
+        {!mobile && (
+          <div className={styles.cardInfo__left}>
+            <img src={poster} alt="poster" />
+          </div>
+        )}
+
         <div className={styles.cardInfoRight}>
           {/* <div className={styles.cardInfoRight__breadcrumbs}></div> */}
           <div className={styles.cardInfoRight__top}>
@@ -49,24 +56,45 @@ const MainInfoCard = ({
               <Title text={title} />
             </div>
             <div className={styles.cardInfoRight__subtitle}>{slogan}</div>
+            {mobile && (
+              <div className={styles.cardInfo__left}>
+                <img src={poster} alt="poster" />
+                {!mobile && <ProgressCircleRating rating={rating} />}
+              </div>
+            )}
             <div className={styles.cardInfoRight__rating}>
-              <ProgressCircleRating rating={rating} />
+              {mobile && <ProgressCircleRating rating={rating} />}
             </div>
             <div className={styles.cardInfoRight__desc}>{desc}</div>
           </div>
-          <div className={styles.cardInfoRight__bottom}>
-            <div className={styles.cardInfoRight__bottomLeft}>
-              <MovieTrailerButton
-                onClick={showModal}
-                text={"watch the trailer"}
-              />
+          {!tablet && (
+            <div className={styles.cardInfoRight__bottom}>
+              <div className={styles.cardInfoRight__bottomLeft}>
+                <MovieTrailerButton
+                  onClick={showModal}
+                  text={"watch the trailer"}
+                />
+              </div>
+              <div className={styles.cardInfoRight__bottomRight}>
+                <SocialLinks array={icons} />
+              </div>
             </div>
-            <div className={styles.cardInfoRight__bottomRight}>
-              <SocialLinks array={icons} />
-            </div>
-          </div>
+          )}
         </div>
       </div>
+        {tablet && (
+            <div className={styles.cardInfoRight__bottom}>
+              <div className={styles.cardInfoRight__bottomLeft}>
+                <MovieTrailerButton
+                  onClick={showModal}
+                  text={"watch the trailer"}
+                />
+              </div>
+              <div className={styles.cardInfoRight__bottomRight}>
+                <SocialLinks array={icons} />
+              </div>
+            </div>
+          )}
       <Popup isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
         <iframe
           width="100%"
